13bdbef47f024c590d9d3208efd5726719e4c4dc
pranitmane <manepranit10@gmail.com>
2024-07-02 17:54:04 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <main className="w-[400px] h-[400px]">
+      <h1 className="text-xl font-bold underline">
+        Hello world!
+      </h1>
+      <div className="flex w-full h-full items-center bg-yellow-300 justify-center">
+        <button className="p-2 border border-gray-600 bg-gray-300 h-fit w-fit">play/pause</button>
+      </div>
+    </main>
+  )
+}
+
+export default App
