1a757353f8633e9bb637827ef27f0a3de0a7f04a
pranitmane <manepranit10@gmail.com>
2024-07-04 21:49:40 +0530

    configured multiple entry files for rollup

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build:{
+    rollupOptions:{
+      input:{
+        main: 'src/main.tsx',
+        background: 'src/background/background.ts',
+        content: 'src/content/content.ts',
+      },
+      output:{
+        entryFileNames: '[name].js',
+      }
+    }
+  }
 })
